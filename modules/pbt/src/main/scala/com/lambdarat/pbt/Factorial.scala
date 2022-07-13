eeceed7c74f3e5b1bd14eca0ca1a3dfe171de1fc
Roberto Serrano <r.semarcos@gmail.com>
2022-07-13 23:34:35 +0200

    Add factorial implementation and suite with examples for it

--- modules/pbt/src/main/scala/com/lambdarat/pbt/Factorial.scala
+++ modules/pbt/src/main/scala/com/lambdarat/pbt/Factorial.scala
@@ -1,5 +1,7 @@
 package com.lambdarat.pbt
 
+import scala.annotation.tailrec
+
 object Factorial {
 
   def of(number: Long): Long = number match {
@@ -7,6 +9,19 @@ object Factorial {
     case 0  => 0
     case 1  => 1
     case 4  => 24
+  }
+
+  implicit class FactorialOps(number: Long) {
+
+    def factorial: Long = {
+      @tailrec
+      def factorialRec(acc: Long, next: Long): Long =
+        if (next == 1) acc
+        else factorialRec(acc * next, next - 1)
+
+      if (number <= 0) 0
+      else factorialRec(1, number)
+    }
   }
 
 }
